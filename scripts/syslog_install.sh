14ec17c301c0e72ee30169b5809ed7056d635e3d
Shivansh Singh <sshvans@amazon.com>
2017-10-19 15:11:53 -0700

    syslog-ng updates for pan log sourcetype

--- scripts/syslog_install.sh
+++ scripts/syslog_install.sh
@@ -8,24 +8,24 @@ yum update -y aws-cfn-bootstrap
 
 # Environment variables
 export SPLUNK_USER=splunk
-export SPLUNK_BIN=/opt/splunk/bin/splunk
-export SPLUNK_HOME=/opt/splunk
+export SPLUNK_BIN=/opt/splunkforwarder/bin/splunk
+export SPLUNK_HOME=/opt/splunkforwarder
 export PATH=$PATH:/opt/aws/bin
-export APPDIR=/opt/splunk/etc/apps/pan-splunk-syslog/default
-export APPDIRROOT=/opt/splunk/etc/apps/pan-splunk-syslog/
+export APPDIR=/opt/splunkforwarder/etc/apps/pan-splunk-syslog/default
+export APPDIRROOT=/opt/splunkforwarder/etc/apps/pan-splunk-syslog/
 
-# Stop Splunk and reset password
-service splunk stop
-touch $SPLUNK_HOME/etc/.ui_login
-mv $SPLUNK_HOME/etc/passwd $SPLUNK_HOME/etc/passwd.bak
+# Stop Splunk, install the splunk forwarder, and reset password
+/opt/splunk/bin/splunk stop
+
+#install splunk universal forwarder
+rpm -i /tmp/splunk-uf.rpm 
+
 sed -i 's/force-change-pass true//' /etc/init.d/splunk
 sudo -u $SPLUNK_USER $SPLUNK_BIN edit user admin -password {{SPLUNK_ADMIN_PASSWORD}} -role admin -auth admin:changeme
 
-chown $SPLUNK_USER:$SPLUNK_USER $SPLUNK_HOME/etc/system/local/server.conf
+#chown $SPLUNK_USER:$SPLUNK_USER $SPLUNK_HOME/etc/system/local/server.conf
+# install syslog-ng and configure init
 
-
-# remove original rsyslog, install syslog-ng, and configure init
-rpm -e --nodeps rsyslog
 yum -y install --enablerepo=epel syslog-ng syslog-ng-libdbi
 chkconfig --add syslog-ng
 chkconfig syslog-ng on
@@ -35,13 +35,16 @@ mkdir -p /var/log/syslog-ng
 service syslog-ng start
 
 # run templating on the splunk configs
-tar xvf /tmp/splunk-pan-syslog-app.tar.gz -C /opt/splunk/etc/apps && rm -f /tmp/splunk-pan-syslog-app.tar.gz
+tar xvf /tmp/splunk-pan-syslog-app.tar.gz -C /opt/splunkforwarder/etc/apps && rm -f /tmp/splunk-pan-syslog-app.tar.gz
 cat $APPDIR/outputs.conf | /tmp/mo > $APPDIR/outputs.conf.new
 mv $APPDIR/outputs.conf.new $APPDIR/outputs.conf
 
 chown -R $SPLUNK_USER:$SPLUNK_USER $APPDIRROOT
 
-service splunk start
+#start splunk
+$SPLUNK_BIN start --accept-license
+
+
 cfn-signal -s true --stack $AWSSTACKNAME --resource SplunkSyslogNodesASG --region $AWSREGION
 
 # Disable splunk user login
